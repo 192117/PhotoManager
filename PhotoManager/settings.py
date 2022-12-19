1d112da991a8a07bff1b0f14d7ca3ed69cf96325
arturkalimullin <noti1996@gmail.com>
2022-12-19 12:15:44 +0300

    Add drf_spectacular for swagger and redoc.

--- PhotoManager/settings.py
+++ PhotoManager/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MY_APPS = [
@@ -87,7 +88,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 # Database
@@ -149,3 +151,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'PhotoManager',
+    'DESCRIPTION': 'API for PhotoManager',
+    'CONTACT': {'email': 'noti1996@gmail.com'},
+    'VERSION': '0.1.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
